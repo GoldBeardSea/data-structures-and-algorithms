dcc81d6185dfa26f81db285d7d9775f8e82abf42
Timothy Joseph Busch <timbusch1@gmail.com>
2019-05-11 22:08:01 -0700

    passes 8

--- includes-charcode/challenges-13.test.js
+++ includes-charcode/challenges-13.test.js
@@ -95,7 +95,7 @@ For example, [['Brook Testing', 'Actual Person'], ['Human Person', 'Brook again'
 ------------------------------------------------------------------------------------------------ */
 
 const unenrollBrook = (arr) => {
-  // Solution code here...
+  return arr.map(arrayOfString => arrayOfString.filter(string => !(string.includes('Brook'))));
 };
 
 /* ------------------------------------------------------------------------------------------------
