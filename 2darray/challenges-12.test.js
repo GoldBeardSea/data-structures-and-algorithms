7ac542d00997bc6c201853e0d9ff0d04b8cfe863
Timothy Joseph Busch <timbusch1@gmail.com>
2019-05-11 22:16:23 -0700

    passed 3

--- 2darray/challenges-12.test.js
+++ 2darray/challenges-12.test.js
@@ -78,7 +78,7 @@ const errands = [
 ];
 
 const howManyTreats = (arr) => {
-  // Solution code here...
+  return arr[2].items[1].quantity;
 };
 
 /* ------------------------------------------------------------------------------------------------
